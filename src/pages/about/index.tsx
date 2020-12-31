466ee5a849a6f6669a8cde3e9f6fe0226fb133f5
Hisho <>
2021-01-01 04:03:14 +0900

    ✨ import relative path

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import htmlWebpackPlugin from 'html-webpack-plugin';
+import {pathType} from "../../config";
 import {myRenderToStaticMarkup} from "../../hooks";
 import {Layout} from "../../layouts";
 import {CurrentPage} from "../../config";
 import {App} from "../../components";
 
-export default () => {
+export default ({htmlWebpackPlugin}: { htmlWebpackPlugin: htmlWebpackPlugin.ProcessedOptions }) => {
+  const relativePath: pathType = htmlWebpackPlugin.options.relativePath;
   return myRenderToStaticMarkup(
-    <CurrentPage.Provider value={{path: '../'} as const}>
+    <CurrentPage.Provider value={{path: relativePath}}>
       <Layout>
         about
         <App/>
