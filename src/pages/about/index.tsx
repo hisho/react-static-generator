b632e340046636c726b55b11d057c52b904f32f9
Hisho <>
2021-01-01 02:13:11 +0900

    ✨ improt App.tsx

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {myRenderToStaticMarkup} from "../../hooks";
 import {Layout} from "../../layouts";
 import {CurrentPage} from "../../config";
+import {App} from "../../components";
 
 export default () => {
   return myRenderToStaticMarkup(
     <CurrentPage.Provider value={{path: '../'} as const}>
       <Layout>
         about
+        <App/>
       </Layout>
     </CurrentPage.Provider>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {myRenderToStaticMarkup} from "../hooks";
 import {Layout} from "../layouts";
 import {CurrentPage} from "../config";
+import {App} from "../components";
 
 export default () => {
   return myRenderToStaticMarkup(
     <CurrentPage.Provider value={{path: './'}}>
       <Layout>
         テスト
+        <App/>
       </Layout>
     </CurrentPage.Provider>
   )
