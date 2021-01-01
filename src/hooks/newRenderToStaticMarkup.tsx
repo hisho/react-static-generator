38aaa5ec2ff9622234097fae71ee9f0256fa1a58
Hisho <>
2021-01-02 00:14:44 +0900

    ✨ add react helmet

--- src/hooks/newRenderToStaticMarkup.tsx
+++ src/hooks/newRenderToStaticMarkup.tsx
@@ -1,11 +1,28 @@
 import {renderToStaticMarkup} from 'react-dom/server';
 import React, {ReactElement} from "react";
+import {Helmet} from "react-helmet";
 import {ProcessedOptions} from 'html-webpack-plugin';
-import {pathType} from "src/config";
-import {CurrentPage} from "src/config";
+import {CurrentPage, pathType} from "src/config";
 
 function myRenderToStaticMarkup(element: ReactElement): string {
-  return '<!DOCTYPE html>' + renderToStaticMarkup(element);
+  const staticMarkup = renderToStaticMarkup(element);
+  const helmet = Helmet.renderStatic();
+
+  function replaceDataHelmet(helmet: string) {
+    return helmet.replace(/data-react-helmet="true"/g, '')
+  }
+
+  return `
+    <!doctype html>
+     <html ${helmet.htmlAttributes.toString()}>
+      <head>
+       ${replaceDataHelmet(helmet.title.toString())}
+       ${replaceDataHelmet(helmet.meta.toString())}
+       ${replaceDataHelmet(helmet.link.toString())}
+      </head>
+      ${staticMarkup}
+    </html>
+  `;
 }
 
 export function newRenderToStaticMarkup(element: ReactElement) {
